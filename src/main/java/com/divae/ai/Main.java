29967df42325b91a4f80269a7062ccdb7c45bab3
Christian Malak <christian.malak@diva-e.com>
2023-01-09 11:13:39 +0100

    Update Main.java

--- src/main/java/com/divae/ai/Main.java
+++ src/main/java/com/divae/ai/Main.java
@@ -1,11 +1,12 @@
 package com.divae.ai;
 
+import com.divae.ai.stages.or.training.OrTrainingground;
 import com.divae.ai.stages.xor.training.XorTrainingground;
 
 public class Main {
     public static void main(String[] args) {
-        //OrTrainingground orTrainingground = new OrTrainingground();
+        OrTrainingground orTrainingground = new OrTrainingground();
 
-        XorTrainingground xorTrainingground = new XorTrainingground();
+        //XorTrainingground xorTrainingground = new XorTrainingground();
     }
 }
